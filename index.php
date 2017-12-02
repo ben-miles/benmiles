ecabc38edd7249e2d662e366a3d71d7069e1d556
Ben <bcgm3@bcgm3.com>
2017-12-02 16:21:50 -0500

    Improvement: btn-toolbar on mobile
    The .btn-toolbar in the portfolio section was breaking out of the page
    on mobile. Changed markup and css to present .btn-group label spans
    full-width on mobile, and inline on larger devices.

--- index.php
+++ index.php
@@ -4,7 +4,15 @@
 // TODO: mobile, fixed background in about section needs to scroll/position better
 // TODO: mobile, about: timeline needs to go full width
 // TODO: SVG logo
+// TODO: menubar font gray(?) on load, fade to black on scroll
 // TODO: SEO
+// TODO: Break sections out into individual php files
+// TODO: mobile, intro, increase vertical spacing between elements. social icons should be at the bottom, account for address bar
+// TODO: clear out unused images
+// TODO: smaller thumbnails for mobile
+// TODO: normalize gallery item heights to prevent big gaps
+
+
 
 // error_reporting(0);
 require 'assets/data.php';
@@ -75,39 +83,43 @@ foreach( $arrayPortfolio as $arrayPortfolioPiece ){
     </div>
     <!-- / section-header -->
 
-    <!-- controls -->
-    <div class="row" id="row-buttons">
-        <div class="col">
-            <div class="btn-toolbar" role="toolbar" aria-label="Gallery Toolbar">
-                <div class="btn-group btn-group-filter mr-3 animated" role="group" aria-label="Gallery Filter Buttons" data-animation="fadeIn" style="animation-delay: 0.2s;">
-                    <span class="input-group-addon">Show:&nbsp;</span>
-                    <button type="button" class="btn btn-secondary active" data-filter=".featured">Featured</button>
-                    <button type="button" class="btn btn-secondary" data-filter=".design">Design</button>
-                    <button type="button" class="btn btn-secondary" data-filter=".development">Development</button>
-                    <button type="button" class="btn btn-secondary" data-filter=".diy">DIY</button>
-                    <button type="button" class="btn btn-secondary" data-filter="*">All</button>
-                </div>
-                <div class="btn-group btn-group-sort mr-3 animated" role="group" aria-label="Gallery Sort Buttons" data-animation="fadeIn" style="animation-delay: 0.2s;">
-                    <span class="input-group-addon">Sort:&nbsp;</span>
-                    <!-- <button type="button" class="btn active btn-secondary" data-sort="original-order">Original</button> -->
-                    <button type="button" class="btn btn-secondary active" data-sort="date">Date</button>
-                    <button type="button" class="btn btn-secondary" data-sort="title">Title</button>
-                    <button type="button" class="btn btn-secondary" id="shuffle">Random</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- / controls -->
-
-    <!-- gallery -->
+    <!-- section-body -->
     <div class="section-body">
         <div class="container">
+
+            <!-- controls -->
+            <div class="row" id="row-buttons">
+                <div class="col">
+                    <div class="btn-toolbar" role="toolbar" aria-label="Gallery Toolbar">
+                        <div class="btn-group btn-group-filter animated" role="group" aria-label="Gallery Filter Buttons" data-animation="fadeIn" style="animation-delay: 0.2s;">
+                            <span class="input-group-addon">Show:&nbsp;</span>
+                            <button type="button" class="btn btn-secondary active" data-filter=".featured">Featured</button>
+                            <button type="button" class="btn btn-secondary" data-filter=".design">Design</button>
+                            <button type="button" class="btn btn-secondary" data-filter=".development">Development</button>
+                            <button type="button" class="btn btn-secondary" data-filter=".diy">DIY</button>
+                            <button type="button" class="btn btn-secondary" data-filter="*">All</button>
+                        </div>
+                        <div class="btn-group btn-group-sort animated" role="group" aria-label="Gallery Sort Buttons" data-animation="fadeIn" style="animation-delay: 0.2s;">
+                            <span class="input-group-addon">Sort:&nbsp;</span>
+                            <!-- <button type="button" class="btn active btn-secondary" data-sort="original-order">Original</button> -->
+                            <button type="button" class="btn btn-secondary active" data-sort="date">Date</button>
+                            <button type="button" class="btn btn-secondary" data-sort="title">Title</button>
+                            <button type="button" class="btn btn-secondary" id="shuffle">Random</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- / controls -->
+
+            <!-- gallery -->
             <div class="row animated" id="gallery" data-animation="fadeIn" style="animation-delay: 0.4s;">
                 <?php echo $output; ?>
             </div>
+            <!-- / gallery -->
+
         </div>
     </div>
-    <!-- / gallery -->
+    <!-- / section-body -->
 
 </section>
 <!-- / port'o'folio -->
